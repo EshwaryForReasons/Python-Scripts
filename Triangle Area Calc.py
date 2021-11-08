1d21c935e57661365fa893db3448500daba09a51
HavenRemix <havenremixgd@gmail.com>
2021-11-08 00:26:04 -0800

    Initial files from 'BS CLass'

---
+++ Triangle Area Calc.py
@@ -0,0 +1,52 @@
+from time import sleep
+
+print("TRIANGLE CALC! WEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE!")
+
+sleep(1)
+
+print("What is the height of the traiangle that you somehow possess?")
+height = input("")
+
+sleep(1)
+
+print("Ha! Now tell me if the base length!")
+length = input("")
+
+sleep(1)
+
+#print("Now to test that you are not a robot: type 'q'")
+
+#test = input("")
+#tests = str(test)
+
+#if test == "q":
+heighti = int(height)
+widthi = int(length)
+
+    #MATHSSSS!
+area = (heighti * widthi) / 2
+print(area = " - is the area!")
+
+sleep(2)
+
+print("BTW this was a dumb question, ask a harder one next time . . .")
+
+sleep(1)
+
+print("it was so bad, I am going to commit suicide!")
+
+sleep(0.5)
+
+print("Bye world!")
+
+quit()
+#else:
+   # print("Bruh you cant even type 'q'? man thats sad . . .")
+
+   # print("it was so sad, I am going to commit suicide!")
+
+  #  sleep(5)
+
+   # print("Bye world!")
+
+    #quit()
---
+++ sentence.py
@@ -0,0 +1,13 @@
+from time import sleep
+
+print("Hello")
+
+w1 = str(input("Gime your first word (name): "))
+w2 = str(input("Second (date)?"))
+w3 = str(input("Third (verb)?"))
+w4 = str(input("Forth (adverd)?"))
+w5 = str(input("Fifth (another name)?"))
+
+print("HERES THE SENTENCE:" + "w1" + "on the " + w2 + "," + w4 + w3 + "in front of" + w5)
+
+sleep(10)
